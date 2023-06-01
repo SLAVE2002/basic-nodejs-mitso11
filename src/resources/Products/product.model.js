080bf4e99234630161ec480769f6ce09a3cd6871
SLAVE2002 <oksikilnika@gmail.com>
2023-06-02 00:24:53 +0300

    update

---
+++ src/resources/Products/product.model.js
@@ -0,0 +1,24 @@
+const { v4: uuid } = require('uuid');
+
+class Product {
+  constructor({ 
+    id = uuid(),
+    name = 'cola',
+    price = '0',
+    ageOflssue= '02',
+    lifeTime = '34',
+  } = {}) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.ageOflssue = ageOflssue;
+    this.lifeTime = lifeTime;
+  }
+
+  static toResponse(product) {
+    const { id, name, price, ageOflssue, lifeTime } = product;
+    return { id, name, price, ageOflssue, lifeTime };
+  }
+}
+
+module.exports = Product;
