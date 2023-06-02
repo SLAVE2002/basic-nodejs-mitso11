c9e767e3f049507b672aad0702772e37a6483a42
SLAVE2002 <oksikilnika@gmail.com>
2023-06-02 03:10:20 +0300

    клиенты ротэйты

--- src/resources/clients/client.router.js
+++ src/resources/clients/client.router.js
@@ -1,14 +1,142 @@
-import { Router } from 'express';
+const productRepository = require('./product.memory.repository');
+const OrderRepository = require('../Orders/orders.memory.repository');
 
-import User from './user.model.js';
-import * as usersService from './client.service.js';
+const getAll = () => productRepository.getAll();
+const getById = (id) => productRepository.getById(id);
+const createProduct = ({
+  id,
+  name, 
+  price, 
+  ageOflssue, 
+  lifeTime,
+}) =>
+productRepository.createProduct({
+    id,
+    name, 
+    price, 
+    ageOflssue, 
+    lifeTime,
+  });
+const deleteById = async (id) => {
+  const productDeletable = await getById(id);
+  productRepository.deleteById(id);
+  OrderRepository.deleteByProductId(id);
+return productDeletable;
+}
 
-const router = Router();
+const updateById = ({
+  id,
+  name, 
+  price, 
+  ageOflssue, 
+  lifeTime,
+}) =>
+productRepository.updateById({
+    id,
+    name, 
+    price, 
+    ageOflssue, 
+    lifeTime,
+  });
 
-router.route('/').get(async (req, res) => {
-  const users = await usersService.getAll();
-  // map user fields to exclude secret fields like "password"
-  res.json(users.map(User.toResponse));
-});
+module.exports = { getAll, getById, createProduct, deleteById, updateById };
+const ClientRepository = require('./client.memory.repository');
+const OrderRepository = require('../Orders/orders.memory.repository');
 
-export default router;
+const getAll = () => ClientRepository.getAll();
+const getById = (id) => ClientRepository.getById(id);
+const createClient = ({ name, adress, bonucecard, numberphone }) =>
+ClientRepository.createClient({ name, adress, bonucecard, numberphone });
+const deleteById = async (id) => {
+  const clientDeletable = await getById(id);
+  ClientRepository.deleteById(id);
+  OrderRepository.removeClientById(id);
+
+  return clientDeletable;
+};
+const updateById = ({ name, adress, bonucecard, numberphone }) =>
+ClientRepository.updateById({ name, adress, bonucecard, numberphone });
+
+module.exports = { getAll, getById, createClient, deleteById, updateById };
+const { StatusCodes } = require('http-status-codes');
+const router = require('express').Router();
+const Client = require('./client.model');
+
+const clientsService = require('./client.service');
+const catchErrors = require('../../common/catchErrors');
+
+router.route('/').get(
+  catchErrors(async (req, res) => {
+    const clients = await clientsService.getAll();
+
+    res.json(clients.map(Client.toResponse));
+  })
+);
+
+router.route('/').post(
+  catchErrors(async (req, res) => {
+    const { name, adress, bonucecard, numberphone } = req.body;
+
+    const client = await clientsService.createClient({ name, adress, bonucecard, numberphone });
+
+    if (client) {
+      res.status(StatusCodes.CREATED).json(Client.toResponse(client));
+    } else {
+      res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ code: 'CLIENT_NOT_CREATE', msg: 'Client not create' });
+    }
+  })
+);
+
+router.route('/:id').get(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+
+    const client = await clientsService.getById(id);
+
+    if (client) {
+      res.json(Client.toResponse(client));
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'CLIENT_NOT_FOUND', msg: 'Client not found' });
+    }
+  })
+);
+
+router.route('/:id').put(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+    const { name, adress, bonucecard, numberphone } = req.body;
+
+    const client = await clientsService.updateById({ name, adress, bonucecard, numberphone});
+
+    if (client) {
+      res.status(StatusCodes.OK).json(Client.toResponse(client));
+    } else {
+      res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'CLIENT_NOT_FOUND', msg: 'Client not found' });
+    }
+  })
+);
+router.route('/:id').delete(
+  catchErrors(async (req, res) => {
+    const { id } = req.params;
+
+    const client = await clientsService.deleteById(id);
+
+    if (!client) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ code: 'CLIENT_NOT_FOUND', msg: 'Client not found' });
+    }
+
+    return res
+      .status(StatusCodes.NO_CONTENT)
+      .json({ code: 'CLIENT_DELETED', msg: 'The client has been deleted' });
+  })
+);
+
+module.exports = router;
