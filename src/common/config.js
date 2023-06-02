74d34dc409040737ed240562cd08fe41d16589b8
SLAVE2002 <oksikilnika@gmail.com>
2023-06-02 03:12:29 +0300

    редакт конфига

--- src/common/config.js
+++ src/common/config.js
@@ -3,16 +3,16 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 // environment
-const NODE_ENV = process.env.NODE_ENV || 'development';
+const NODE_ENV = process.env.NODE_ENV  'development';
 
 // application
-const PORT = +process.env.PORT || 4000;
+const PORT = +process.env.PORT  4000;
 
 // JWT
-const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY || 'secret-key';
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY  'jarmag2018';
 
 // database
 const MONGO_CONNECTION_STRING =
-  process.env.MONGO_CONNECTION_STRING || 'your-mongo-db-connection-string';
+  process.env.MONGO_CONNECTION_STRING  'MONGO_CONNECTION_STRING';
 
 export { PORT, NODE_ENV, MONGO_CONNECTION_STRING, JWT_SECRET_KEY };
